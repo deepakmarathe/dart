39ba4a975244d39f4e6dd5b5f40bacabaeac915f
deepak marathe <dpkmarathe@gmail.com>
2020-07-13 12:46:33 +0530

    add factory for shape as option 2

--- lib/src/codelab.java_to_dart/shape.dart
+++ lib/src/codelab.java_to_dart/shape.dart
@@ -8,7 +8,11 @@ Shape shapeFactory(String type) {
 
 abstract class Shape{
   num get area;
-
+  factory Shape(String type) {
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Can\'t create $type.';
+  }
 }
 
 class Circle implements Shape {
