d2d88e28d4849d1e5aecd3b102464b57f9643dda
deepak marathe <dpkmarathe@gmail.com>
2020-07-13 16:05:09 +0530

    functional programming 1

--- lib/src/codelab.java_to_dart/shape.dart
+++ lib/src/codelab.java_to_dart/shape.dart
@@ -47,7 +47,8 @@ int main() {
 
   //functional programming
   final values = [1, 2, 3, 5, 10, 50];
-  values.map((e) => scream(e)).forEach((element) => {print(element)});
+//  values.map((e) => scream(e)).forEach((element) => {print(element)});
+  values.skip(1).take(3).map(scream).forEach(print);
 
 //  for(var length in values){
 //    print(scream(length));
